8b9dde2b97c7e221481e01606cafb34e184dd487
zac <z.t.lewis@lavabit.com>
2020-04-05 20:19:55 +1000

    Parse a 'variable' block.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 #[macro_use] extern crate lalrpop_util;
 
-lalrpop_mod!(pub calculator1); // synthesized by LALRPOP
+lalrpop_mod!(pub hcl); // synthesized by LALRPOP
 
 #[test]
-fn calculator1() {
-	assert!(calculator1::TermParser::new().parse("((22))").is_ok());
+fn hcl() {
+    let ml = r#"
+variable "hello" {
+  type = string
+}
+"#;
+    assert!(hcl::StructureParser::new().parse(ml).is_ok());
 }
 
 fn main() {
-    println!("{:?}", calculator1::TermParser::new().parse("((22))").unwrap());
     println!("Hello, world!");
 }
